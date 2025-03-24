fd160efed599b1212693e591a9e0e145501add8c
jessedahlke <jesse.dahlke@ndsu.edu>
2025-03-24 11:51:12 -0500

    Began working with services

---
+++ src/app/logging.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+
+  constructor() { }
+
+  onStatusChange(status: string) {
+    console.log(`A status change occured, the new status is ${status}`);
+  }
+}
